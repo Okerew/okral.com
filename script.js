b7ddac3e87e3647f058ffcba7b7453eff8e85e01
Glitch (okral) <none>
2024-04-02 17:13:59 +0000

    Updated with Glitch

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+function autoclicker() {
+        var x = document.getElementById("autoclicker");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+ 
+function spammer() {
+        var x = document.getElementById("spammer");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+ 
+function pyos() {
+        var x = document.getElementById("pyos");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
+function dptech() {
+        var x = document.getElementById("dptech");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
+function ltsm() {
+        var x = document.getElementById("ltsm");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
+function ok_l() {
+        var x = document.getElementById("ok_l");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
