480217128329b0d6c727aa45dedf14f6e1d0efc9
Okerew <93822247+Okerew@users.noreply.github.com>
2025-07-10 17:07:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,23 +26,10 @@ function ltsm() {
   toggleVisibility("ltsm");
 }
 
-function ok_l() {
-  toggleVisibility("ok_l");
-}
-
 function oce() {
   toggleVisibility("oce");
 }
-function okrol() {
-  toggleVisibility("okrol");
-}
-function okterminal() {
-  toggleVisibility("okterminal");
-}
 
-function ok_blockchain() {
-  toggleVisibility("okblockchain");
-}
 function oklearn() {
   toggleVisibility("oklearn");
 }
